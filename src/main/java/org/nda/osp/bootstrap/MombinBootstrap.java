9b6888e62ebe618692f6f7599f9723a961d4df83
Dmitry Nikiforov <dnikiforov@mail.ru>
2024-11-12 22:01:31 +0300

    Append logback and inital bootstrapping

--- src/main/java/org/nda/osp/bootstrap/MombinBootstrap.java
+++ src/main/java/org/nda/osp/bootstrap/MombinBootstrap.java
@@ -1,10 +1,23 @@
 package org.nda.osp.bootstrap;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
 @CommandLine.Command
 public class MombinBootstrap implements Runnable {
+
+    private static final int PORT = 9778;
+
+    private static Logger log = LoggerFactory.getLogger(MombinBootstrap.class);
 
     @CommandLine.Option(names = "--port")
     private Integer port;
@@ -16,28 +29,31 @@ public class MombinBootstrap implements Runnable {
     @Override
     public void run() {
         if (port == null) {
-            port = 1024;
+            port = PORT;
         }
-        System.out.println("POrt is "+port);
-        /*EventLoopGroup bossGroup = new NioEventLoopGroup();
+        log.info("Port is {}", port);
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         public void initChannel(SocketChannel ch)
                                 throws Exception {
+
                         }
                     }).option(ChannelOption.SO_BACKLOG, 128)
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
 
-            ChannelFuture f = b.bind(9676).sync();
+            ChannelFuture f = bootstrap.bind(port).sync();
             f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         } finally {
             workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
-        }*/
+        }
     }
 }
