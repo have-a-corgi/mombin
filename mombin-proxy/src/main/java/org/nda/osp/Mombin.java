334da4d31fc272422bb5c4110fecaafdb2355d0c
Dmitry Nikiforov <dnikiforov@mail.ru>
2024-11-21 00:06:02 +0300

    Modify

---
+++ mombin-proxy/src/main/java/org/nda/osp/Mombin.java
@@ -0,0 +1,13 @@
+package org.nda.osp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mombin {
+
+    public static void main(String... args) {
+        SpringApplication.run(Mombin.class, args);
+    }
+
+}
